5f965bf0ed6eccd2c480673cd089314318bb4fb7
Jakob Stjernholm <jakob.stjerna@gmail.com>
2016-01-27 10:41:56 +0100

    Done with laboration 3.

--- EmployeeWinFormClient/Form1.Designer.cs
+++ EmployeeWinFormClient/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmployeeWinFormClient
 {
-    partial class Form1
+    partial class btnAdd
     {
         /// <summary>
         /// Required designer variable.
@@ -31,6 +31,34 @@
             this.listView1 = new System.Windows.Forms.ListView();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.btnGetEmployee = new System.Windows.Forms.Button();
+            this.lblAddEmployee = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFirstname = new System.Windows.Forms.TextBox();
+            this.textBoxLastname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxCourtesy = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxBirthdate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxHiredate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxCity = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxRegion = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxPostalCode = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxCountry = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBoxHomePhone = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            this.lblErrorMessageGet = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,18 +88,274 @@
             this.btnGetEmployee.UseVisualStyleBackColor = true;
             this.btnGetEmployee.Click += new System.EventHandler(this.btnGetEmployee_Click);
             // 
-            // Form1
+            // lblAddEmployee
+            // 
+            this.lblAddEmployee.AutoSize = true;
+            this.lblAddEmployee.Location = new System.Drawing.Point(533, 18);
+            this.lblAddEmployee.Name = "lblAddEmployee";
+            this.lblAddEmployee.Size = new System.Drawing.Size(99, 17);
+            this.lblAddEmployee.TabIndex = 3;
+            this.lblAddEmployee.Text = "Add Employee";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(421, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Firstname";
+            // 
+            // textBoxFirstname
+            // 
+            this.textBoxFirstname.Location = new System.Drawing.Point(536, 40);
+            this.textBoxFirstname.Name = "textBoxFirstname";
+            this.textBoxFirstname.Size = new System.Drawing.Size(168, 22);
+            this.textBoxFirstname.TabIndex = 5;
+            // 
+            // textBoxLastname
+            // 
+            this.textBoxLastname.Location = new System.Drawing.Point(536, 68);
+            this.textBoxLastname.Name = "textBoxLastname";
+            this.textBoxLastname.Size = new System.Drawing.Size(168, 22);
+            this.textBoxLastname.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(421, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Lastname";
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(536, 96);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(168, 22);
+            this.textBoxTitle.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Title";
+            // 
+            // textBoxCourtesy
+            // 
+            this.textBoxCourtesy.Location = new System.Drawing.Point(536, 124);
+            this.textBoxCourtesy.Name = "textBoxCourtesy";
+            this.textBoxCourtesy.Size = new System.Drawing.Size(168, 22);
+            this.textBoxCourtesy.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(421, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Title of courtesy";
+            // 
+            // textBoxBirthdate
+            // 
+            this.textBoxBirthdate.Location = new System.Drawing.Point(536, 152);
+            this.textBoxBirthdate.Name = "textBoxBirthdate";
+            this.textBoxBirthdate.Size = new System.Drawing.Size(168, 22);
+            this.textBoxBirthdate.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(421, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 17);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Birthdate";
+            // 
+            // textBoxHiredate
+            // 
+            this.textBoxHiredate.Location = new System.Drawing.Point(536, 180);
+            this.textBoxHiredate.Name = "textBoxHiredate";
+            this.textBoxHiredate.Size = new System.Drawing.Size(168, 22);
+            this.textBoxHiredate.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(421, 183);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Hiredate";
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(536, 208);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(168, 22);
+            this.textBoxAddress.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(421, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Address";
+            // 
+            // textBoxCity
+            // 
+            this.textBoxCity.Location = new System.Drawing.Point(536, 236);
+            this.textBoxCity.Name = "textBoxCity";
+            this.textBoxCity.Size = new System.Drawing.Size(168, 22);
+            this.textBoxCity.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(421, 239);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "City";
+            // 
+            // textBoxRegion
+            // 
+            this.textBoxRegion.Location = new System.Drawing.Point(536, 264);
+            this.textBoxRegion.Name = "textBoxRegion";
+            this.textBoxRegion.Size = new System.Drawing.Size(168, 22);
+            this.textBoxRegion.TabIndex = 21;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(421, 267);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 17);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Region";
+            // 
+            // textBoxPostalCode
+            // 
+            this.textBoxPostalCode.Location = new System.Drawing.Point(536, 292);
+            this.textBoxPostalCode.Name = "textBoxPostalCode";
+            this.textBoxPostalCode.Size = new System.Drawing.Size(168, 22);
+            this.textBoxPostalCode.TabIndex = 23;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(421, 295);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(82, 17);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "Postal code";
+            // 
+            // textBoxCountry
+            // 
+            this.textBoxCountry.Location = new System.Drawing.Point(536, 320);
+            this.textBoxCountry.Name = "textBoxCountry";
+            this.textBoxCountry.Size = new System.Drawing.Size(168, 22);
+            this.textBoxCountry.TabIndex = 25;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(421, 323);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(57, 17);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Country";
+            // 
+            // textBoxHomePhone
+            // 
+            this.textBoxHomePhone.Location = new System.Drawing.Point(536, 348);
+            this.textBoxHomePhone.Name = "textBoxHomePhone";
+            this.textBoxHomePhone.Size = new System.Drawing.Size(168, 22);
+            this.textBoxHomePhone.TabIndex = 27;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(421, 351);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(89, 17);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "Home phone";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(536, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 23);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Add Employee";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.AutoSize = true;
+            this.lblErrorMessage.Location = new System.Drawing.Point(536, 406);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(0, 17);
+            this.lblErrorMessage.TabIndex = 31;
+            // 
+            // lblErrorMessageGet
+            // 
+            this.lblErrorMessageGet.AutoSize = true;
+            this.lblErrorMessageGet.Location = new System.Drawing.Point(13, 295);
+            this.lblErrorMessageGet.Name = "lblErrorMessageGet";
+            this.lblErrorMessageGet.Size = new System.Drawing.Size(0, 17);
+            this.lblErrorMessageGet.TabIndex = 32;
+            // 
+            // btnAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1056, 483);
+            this.Controls.Add(this.lblErrorMessageGet);
+            this.Controls.Add(this.lblErrorMessage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxHomePhone);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBoxCountry);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBoxPostalCode);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxRegion);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxCity);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxHiredate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxBirthdate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxCourtesy);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxLastname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFirstname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblAddEmployee);
             this.Controls.Add(this.btnGetEmployee);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.listView1);
-            this.Name = "Form1";
+            this.Name = "btnAdd";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -80,6 +364,34 @@
         private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Button btnGetEmployee;
+        private System.Windows.Forms.Label lblAddEmployee;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFirstname;
+        private System.Windows.Forms.TextBox textBoxLastname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxCourtesy;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxBirthdate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxHiredate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxAddress;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxCity;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxRegion;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBoxPostalCode;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxCountry;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBoxHomePhone;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblErrorMessage;
+        private System.Windows.Forms.Label lblErrorMessageGet;
     }
 }
 
--- EmployeeWinFormClient/Form1.cs
+++ EmployeeWinFormClient/Form1.cs
@@ -11,39 +11,105 @@ using EmployeeWinFormClient.ServiceReference1;
 
 namespace EmployeeWinFormClient
 {
-    public partial class Form1 : Form
+    public partial class btnAdd : Form
     {
-        public Form1()
+        public btnAdd()
         {
             InitializeComponent();
         }
 
         private void btnGetEmployee_Click(object sender, EventArgs e)
         {
-            EmployeeServiceClient client = new EmployeeServiceClient();
-            List<Employee> result = new List<Employee>();
-            result.Add(client.GetEmployee((int)numericUpDown1.Value));
-                 client.GetEmployee((int) numericUpDown1.Value);
-        
-           
-            foreach (var i in result)
+            try
             {
-            listView1.Items.Add(i.EmployeeID.ToString());
-            listView1.Items.Add(i.Address);
-            listView1.Items.Add(i.City);
-            listView1.Items.Add(i.Country);
-            listView1.Items.Add(i.PostalCode);
-            listView1.Items.Add(i.FirstName);
-            listView1.Items.Add(i.LastName);
-            listView1.Items.Add(i.HomePhone);
-            listView1.Items.Add(i.Region);
-            listView1.Items.Add(i.Title);
-            listView1.Items.Add(i.TitleOfCourtesy);
-            listView1.Items.Add(i.HireDate.ToString());
-            listView1.Items.Add(i.BirthDate.ToString());
+                lblErrorMessageGet.Text = "";
+                listView1.Clear();
+                EmployeeServiceClient client = new EmployeeServiceClient();
+                List<Employee> result = new List<Employee>();
+                result.Add(client.GetEmployee((int)numericUpDown1.Value));
+                client.GetEmployee((int)numericUpDown1.Value);
+
+
+                foreach (var i in result)
+                {
+                    listView1.Items.Add(i.EmployeeID.ToString());
+                    listView1.Items.Add(i.Address);
+                    listView1.Items.Add(i.City);
+                    listView1.Items.Add(i.Country);
+                    listView1.Items.Add(i.PostalCode);
+                    listView1.Items.Add(i.FirstName);
+                    listView1.Items.Add(i.LastName);
+                    listView1.Items.Add(i.HomePhone);
+                    listView1.Items.Add(i.Region);
+                    listView1.Items.Add(i.Title);
+                    listView1.Items.Add(i.TitleOfCourtesy);
+                    listView1.Items.Add(i.HireDate.ToString());
+                    listView1.Items.Add(i.BirthDate.ToString());
+
+                }
+            }
+            catch (Exception exception)
+            {
+                lblErrorMessageGet.Text = exception.Message;
+            }
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblErrorMessage.Text = "";
                 
+                if (DateTime.Parse(textBoxBirthdate.Text) >= new DateTime(1753, 01, 01) &&
+                    DateTime.Parse(textBoxHiredate.Text) >= new DateTime(1753, 01, 01))
+                {
+                    EmployeeServiceClient client = new EmployeeServiceClient();
+                    Employee employee = new Employee
+                    {
+                        Address = textBoxAddress.Text,
+                        BirthDate = DateTime.Parse(textBoxBirthdate.Text),
+                        City = textBoxCity.Text,
+                        Country = textBoxCountry.Text,
+                        FirstName = textBoxFirstname.Text,
+                        HireDate = DateTime.Parse(textBoxHiredate.Text),
+                        HomePhone = textBoxHomePhone.Text,
+                        LastName = textBoxLastname.Text,
+                        PostalCode = textBoxPostalCode.Text,
+                        Region = textBoxRegion.Text,
+                        Title = textBoxTitle.Text,
+                        TitleOfCourtesy = textBoxCourtesy.Text
+                    };
+                    client.AddEmployee(employee);
+                    ClearTextboxes();
+                }
+                else
+                {
+                    lblErrorMessage.Text = "Date can not that early!";
+                }
+            }
+            catch (Exception exception)
+            {
+                lblErrorMessage.Text = exception.Message;
             }
 
+        }
+
+        private void ClearTextboxes()
+        {
+            textBoxBirthdate.Clear();
+            textBoxAddress.Clear();
+            textBoxCity.Clear();
+            textBoxCountry.Clear();
+            textBoxFirstname.Clear();
+            textBoxLastname.Clear();
+            textBoxHiredate.Clear();
+            textBoxHomePhone.Clear();
+            textBoxPostalCode.Clear();
+            textBoxRegion.Clear();
+            textBoxTitle.Clear();
+            textBoxCourtesy.Clear();
         }
     }
 }
